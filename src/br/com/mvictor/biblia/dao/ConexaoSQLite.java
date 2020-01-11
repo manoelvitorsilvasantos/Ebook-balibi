37b13c927d41c19a488613d48776ebdc0e284cc8
mvictor10 <mvictordossantos.limite@gmail.com>
2020-01-11 03:34:24 -0300

    swss

--- src/br/com/mvictor/biblia/dao/ConexaoSQLite.java
+++ src/br/com/mvictor/biblia/dao/ConexaoSQLite.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package conexoes;
+package br.com.mvictor.biblia.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/br/com/mvictor/biblia/view/CriarBancoSQLite.java
+++ src/br/com/mvictor/biblia/view/CriarBancoSQLite.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package programinhassqlite;
+package br.com.mvictor.biblia.view;
 
 import conexoes.ConexaoSQLite;
 import java.sql.SQLException;
